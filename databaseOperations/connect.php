39889691656893b15bd612d6f514d9044806144b
cachume <albertq703@gmail.com>
2025-03-05 22:20:16 -0400

    Inicial

---
+++ databaseOperations/connect.php
@@ -0,0 +1,18 @@
+<?php
+    class ConectionDB{
+        private static $servername = "localhost";
+        private static $database = "sallevent";
+        private static $username = "root";
+        private static $password = "";
+
+        public static function get_connection(): mysqli {
+            $connection = new mysqli(self::$servername, self::$username, self::$password, self::$database);
+            if($connection->connect_error) {
+                die("Connection failed: ".$connection->connect_error);
+            }
+            //echo "Connected successfully";
+            return $connection;
+        }
+        private function __construct() {}
+    }
+?>
